905e53ab3477d5a24289e207ec68155121431d01
jonas.junior <jonas.junior@valemobi.com.br>
2022-03-25 10:09:04 -0300

    ajustando as dependências do spring e criando model da empresa

--- register/src/main/java/com/autog/register/entity/Empresa.java
+++ register/src/main/java/com/autog/register/entity/Empresa.java
@@ -1,4 +1,91 @@
 package com.autog.register.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@Entity
 public class Empresa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idEmpresa;
+
+    @NotBlank
+    private String razãoSocial;
+
+    @NotBlank
+    private String cnpj;
+
+    @NotBlank
+    private String telefone;
+
+    @NotBlank
+    private String email;
+
+    @NotNull
+    private LocalDate dataAbertura;
+
+    @NotNull
+    private Boolean ativa;
+
+    public Integer getIdEmpresa() {
+        return idEmpresa;
+    }
+
+    public void setIdEmpresa(Integer idEmpresa) {
+        this.idEmpresa = idEmpresa;
+    }
+
+    public String getRazãoSocial() {
+        return razãoSocial;
+    }
+
+    public void setRazãoSocial(String razãoSocial) {
+        this.razãoSocial = razãoSocial;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getDataAbertura() {
+        return dataAbertura;
+    }
+
+    public void setDataAbertura(LocalDate dataAbertura) {
+        this.dataAbertura = dataAbertura;
+    }
+
+    public Boolean getAtiva() {
+        return ativa;
+    }
+
+    public void setAtiva(Boolean ativa) {
+        this.ativa = ativa;
+    }
 }
