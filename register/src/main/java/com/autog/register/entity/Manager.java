1f88a4767f563c01ab3a3e59327b0067271a067a
jonas.junior <jonas.junior@valemobi.com.br>
2022-03-25 23:08:18 -0300

    criando relacionamento entre empresa e gestor

---
+++ register/src/main/java/com/autog/register/entity/Manager.java
@@ -0,0 +1,70 @@
+package com.autog.register.entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "Gestor")
+public class Manager {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idGestor")
+    private Integer idManager;
+
+    @NotBlank
+    @Column(name = "nome")
+    private String name;
+
+    @NotBlank
+    @Column(name = "login")
+    private String login;
+
+    @NotBlank
+    @Column(name = "senha")
+    private String password;
+
+    @ManyToOne
+    @JoinColumn(name = "fkEmpresa", referencedColumnName = "idEmpresa")
+    private Company company;
+
+    public Integer getIdManager() {
+        return idManager;
+    }
+
+    public void setIdManager(Integer idManager) {
+        this.idManager = idManager;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+}
