d87459d50a20dfabf96607ecdecfbc6d23b0c648
jonas.junior <jonas.junior@valemobi.com.br>
2022-03-28 10:22:34 -0300

    criando service para company

--- register/src/main/java/com/autog/register/controller/CompanyController.java
+++ register/src/main/java/com/autog/register/controller/CompanyController.java
@@ -2,58 +2,39 @@ package com.autog.register.controller;
 
 import com.autog.register.dto.request.CompanyUpdateData;
 import com.autog.register.entity.Company;
-import com.autog.register.repository.CompanyRepository;
+import com.autog.register.service.CompanyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @RestController
 @RequestMapping("/companies")
 public class CompanyController {
 
     @Autowired
-    private CompanyRepository repository;
+    private CompanyService service;
 
     @PostMapping
     public ResponseEntity registerCompany(@RequestBody @Valid Company newCompany) {
-        repository.save(newCompany);
-        return ResponseEntity.status(201).build();
+        return service.registerCompany(newCompany);
     }
 
     @GetMapping
     public ResponseEntity getCompanies() {
-        List<Company> companies = repository.findAll();
-        if (companies.isEmpty()) {
-            return ResponseEntity.status(204).build();
-        }
-        return ResponseEntity.status(200).body(companies);
+        return service.getCompanies();
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity deleteCompanyById(@PathVariable Integer id) {
-        if (repository.existsById(id)) {
-            repository.deleteCompany(id);
-            return ResponseEntity.status(200).build();
-        }
-        return ResponseEntity.status(404).build();
+        return service.deleteCompanyById(id);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity updateCompany(
             @PathVariable Integer id,
             @RequestBody @Valid CompanyUpdateData request) {
-        if (repository.existsById(id)) {
-            repository.updateCompany(
-                    id,
-                    request.getCorporateName(),
-                    request.getCnpj(),
-                    request.getTelephone(),
-                    request.getEmail());
-            return ResponseEntity.status(200).build();
-        }
-        return ResponseEntity.status(404).build();
+        return service.updateCompany(id, request);
     }
 }
