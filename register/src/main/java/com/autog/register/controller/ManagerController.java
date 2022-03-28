3c6c5c56c828e0b90b2569f3d7dd9e3e6a18a73b
jonas.junior <jonas.junior@valemobi.com.br>
2022-03-28 10:33:59 -0300

    criando service para Manager

--- register/src/main/java/com/autog/register/controller/ManagerController.java
+++ register/src/main/java/com/autog/register/controller/ManagerController.java
@@ -1,51 +1,37 @@
 package com.autog.register.controller;
 
 import com.autog.register.entity.Manager;
-import com.autog.register.repository.ManagerRepository;
+import com.autog.register.service.ManagerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @RestController
 @RequestMapping("/managers")
 public class ManagerController {
 
     @Autowired
-    private ManagerRepository repository;
+    private ManagerService service;
 
     @PostMapping
     public ResponseEntity registerEquipment(@RequestBody @Valid Manager newManager) {
-        repository.save(newManager);
-        return ResponseEntity.status(201).build();
+        return service.registerEquipment(newManager);
     }
 
     @GetMapping
     public ResponseEntity getManagers() {
-        List<Manager> managers = repository.findAll();
-        if (managers.isEmpty()) {
-            return ResponseEntity.status(204).build();
-        }
-        return ResponseEntity.status(200).body(managers);
+        return service.getManagers();
     }
 
     @PatchMapping("/{id}")
     public ResponseEntity editManager(@PathVariable Integer id, @RequestBody @Valid Manager request) {
-        if (repository.existsById(id)) {
-            repository.updateManager(id, request.getName(), request.getLogin(), request.getPassword());
-            return ResponseEntity.status(200).build();
-        }
-        return ResponseEntity.status(404).build();
+        return service.editManager(id, request);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity deleteManagerById(@PathVariable Integer id) {
-        if (repository.existsById(id)) {
-            repository.deleteManager(id);
-            return ResponseEntity.status(200).build();
-        }
-        return ResponseEntity.status(404).build();
+        return service.deleteManagerById(id);
     }
 }
---
+++ register/src/main/java/com/autog/register/service/ManagerService.java
@@ -0,0 +1,45 @@
+package com.autog.register.service;
+
+import com.autog.register.entity.Manager;
+import com.autog.register.repository.ManagerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ManagerService {
+
+    @Autowired
+    private ManagerRepository repository;
+
+    public ResponseEntity registerEquipment(Manager newManager) {
+        repository.save(newManager);
+        return ResponseEntity.status(201).build();
+    }
+
+    public ResponseEntity getManagers() {
+        List<Manager> managers = repository.findAll();
+        if (managers.isEmpty()) {
+            return ResponseEntity.status(204).build();
+        }
+        return ResponseEntity.status(200).body(managers);
+    }
+
+    public ResponseEntity editManager(Integer id, Manager request) {
+        if (repository.existsById(id)) {
+            repository.updateManager(id, request.getName(), request.getLogin(), request.getPassword());
+            return ResponseEntity.status(200).build();
+        }
+        return ResponseEntity.status(404).build();
+    }
+
+    public ResponseEntity deleteManagerById(Integer id) {
+        if (repository.existsById(id)) {
+            repository.deleteManager(id);
+            return ResponseEntity.status(200).build();
+        }
+        return ResponseEntity.status(404).build();
+    }
+}
