31988d0be60eee1994c2630ddaf3373401467de7
jonas.junior <jonas.junior@valemobi.com.br>
2022-06-04 01:45:18 -0300

    ajustando select personalizado com dto

--- src/main/java/com/autog/register/repository/EquipmentRepository.java
+++ src/main/java/com/autog/register/repository/EquipmentRepository.java
@@ -1,5 +1,6 @@
 package com.autog.register.repository;
 
+import com.autog.register.dto.response.EquipmentResponse;
 import com.autog.register.entity.Equipment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -12,6 +13,26 @@ public interface EquipmentRepository extends JpaRepository<Equipment, Integer> {
 
     @Query("SELECT e FROM Equipment e WHERE clnBox.idCLNBox = ?1")
     List<Equipment> getEquipmentByClnBox(Integer idCLNBox);
+
+    @Query(value="SELECT \n" +
+            "s.id_sala,\n" +
+            " s.nome as s_nome," +
+            " s.andar,\n" +
+            "  e.id_equipamento,\n" +
+            "   e.nome, e.tipo, \n" +
+            "   e.instalacao, \n" +
+            "   e.vida_util,\n" +
+            "    e.potencia,\n" +
+            "\t e.qtd_equipamento,\n" +
+            "\t  e.porta\n" +
+            "FROM [dbo].[sala] as s\n" +
+            "INNER JOIN [dbo].[clnbox] as c\n" +
+            "ON s.id_sala = c.fk_sala\n" +
+            "INNER JOIN [dbo].[equipamento] as e\n" +
+            "ON c.id_clnbox = e.fk_clnbox\n" +
+            "WHERE c.id_clnbox = ?1",
+            nativeQuery=true)
+    List<Object[]> getEquipmentAndRoomByClnBox(Integer idCLNBox);
 
     @Transactional
     @Modifying
--- src/main/java/com/autog/register/service/EquipmentService.java
+++ src/main/java/com/autog/register/service/EquipmentService.java
@@ -1,6 +1,7 @@
 package com.autog.register.service;
 
 import com.autog.register.dto.request.EquipmentRequest;
+import com.autog.register.dto.response.EquipmentResponse;
 import com.autog.register.entity.Equipment;
 import com.autog.register.entity.Room;
 import com.autog.register.repository.EquipmentRepository;
@@ -9,7 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 @Service
 public class EquipmentService {
@@ -23,11 +29,37 @@ public class EquipmentService {
     }
 
     public ResponseEntity getEquipment(Integer idCLNBox) {
-        List<Equipment> equipments = repository.getEquipmentByClnBox(idCLNBox);
-        if (equipments.isEmpty()) {
+        List<Object[]> equipments = repository.getEquipmentAndRoomByClnBox(idCLNBox);
+        List<EquipmentResponse> equipmentsToBeReturned = new ArrayList<>();
+//        List<Equipment> equipments = repository.getEquipmentByClnBox(idCLNBox);
+
+        for(Object[] equip : equipments) {
+            equipmentsToBeReturned.add(new EquipmentResponse(
+                    (Integer) equip[0],
+                    (String) equip[1],
+                    (Integer) equip[2],
+                    (Integer) equip[3],
+                    (String) equip[4],
+                    (String) equip[5],
+                    ((Date) equip[6]),
+                    (Integer) equip[7],
+                    (Integer) equip[8],
+                    (Integer) equip[9],
+                    (Integer) equip[10]
+            ));
+        }
+
+        if (equipmentsToBeReturned.isEmpty()) {
             return ResponseEntity.noContent().build();
         }
-        return ResponseEntity.ok(equipments);
+        return ResponseEntity.ok(equipmentsToBeReturned);
+    }
+
+    private LocalDate convertStringToLocalDate(String dateToBeFormatted) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        formatter = formatter.withLocale( Locale.US );
+        LocalDate date = LocalDate.parse(dateToBeFormatted, formatter);
+        return date;
     }
 
     public ResponseEntity editEquipment(Integer id, EquipmentRequest request) {
